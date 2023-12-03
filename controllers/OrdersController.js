1ca39c128bb4aa536fd1acad5ff80b8ad3bdafd6
Madi <havkpzz@gmail.com>
2023-12-03 15:26:51 +0600

    Created orders controller

---
+++ controllers/OrdersController.js
@@ -0,0 +1,69 @@
+const Orders = require("../models/OrdersModel");
+const mongoose = require("mongoose");
+
+const getAllOrderItems = async (req, res) => {
+  try {
+    const data = await Orders.find({}).sort({ createdAt: -1 });
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const getOrderItemsById = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ error: `id: ${id} is not valid` });
+  }
+  const data = await Orders.findById(id);
+  if (!data) {
+    return res.status(404).json({ error: `There is no item with id: ${id}` });
+  }
+  res.status(200).json(data);
+};
+
+const createOrderItems = async (req, res) => {
+  const { title, amount, price, image } = req.body;
+  try {
+    const data = await Orders.create({ title, amount, price, image });
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const updateOrderItems = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ error: `id: ${id} is not valid` });
+  }
+  const data = await Orders.findOneAndUpdate({ _id: id }, { ...req.body });
+  if (!data) {
+    return res
+      .status(404)
+      .json({ error: `There is no item with id: ${id} for updating` });
+  }
+  res.status(200).json(data);
+};
+
+const deleteOrderItems = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ error: `id: ${id} is not valid` });
+  }
+  const data = await Orders.findByIdAndDelete({ _id: id });
+  if (!data) {
+    return res
+      .status(404)
+      .json({ error: `There is no item with id: ${id} for deleting` });
+  }
+  res.status(200).json(data);
+};
+
+module.exports = {
+  getAllOrderItems,
+  getOrderItemsById,
+  createOrderItems,
+  updateOrderItems,
+  deleteOrderItems,
+};
--- routes/orders.js
+++ routes/orders.js
@@ -1,27 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const Orders = require("../models/OrdersModel");
+const {
+  getAllOrderItems,
+  getOrderItemsById,
+  createOrderItems,
+  updateOrderItems,
+  deleteOrderItems,
+} = require("../controllers/OrdersController");
 
-router.get("/", (req, res) => {
-  res.send("main orders");
-});
-router.get("/:id", (req, res) => {
-  res.send("food Orders by id");
-});
-router.post("/", async (req, res) => {
-  const { title, amount, price, image } = req.body;
-  try {
-    const data = await Orders.create({ title, amount, price, image });
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-router.put("/", (req, res) => {
-  res.send("food Orders put");
-});
-router.delete("/", (req, res) => {
-  res.send("food Orders delete");
-});
+router.get("/", getAllOrderItems);
+router.get("/:id", getOrderItemsById);
+router.post("/", createOrderItems);
+router.patch("/:id", updateOrderItems);
+router.delete("/:id", deleteOrderItems);
 
 module.exports = router;
