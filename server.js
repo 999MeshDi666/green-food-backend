5f35fd1ec2ade9ba4944843fa4c31f9e55c21060
madi <havkpzz@gmail.com>
2023-12-20 15:52:10 +0600

    Update static file path

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 const port = process.env.PORT;
 const catalogs = require('./routes/catalogs');
 const orders = require('./routes/orders');
+const path = require('path');
 
 app.use(express.json());
 app.use('/api/catalogs', catalogs);
 app.use('/api/orders', orders);
-app.use(express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
