97372677ed066561bc339c623f03ef502563ce35
madi <havkpzz@gmail.com>
2023-12-25 15:11:33 +0600

    Fix signup problem and add login function, add jwt

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,16 +1,27 @@
 const Users = require('../models/UserModel');
-const mongoose = require('mongoose');
+
+const jwt = require('jsonwebtoken');
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: '30s' });
+};
 
 const loginUser = async (req, res) => {
-  res.json({ msg: 'check login' });
+  const { email, password } = req.body;
+  try {
+    const user = await Users.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 const signupUser = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const user = await Users.signup(username, email, password);
-    console.log(user);
-    console.log(user);
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 const Schema = mongoose.Schema;
 
 const user = new Schema({
@@ -18,20 +19,44 @@ const user = new Schema({
   },
 });
 
-user.statics.signup = async function ({ username, email, password }) {
-  // const existEmail = this.findOne({ email });
-  // const existUsername = this.findOne({ username });
+user.statics.signup = async function (username, email, password) {
+  if (!username || !email || !password) {
+    throw Error('All fields must be filled');
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Filed must be an email');
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password is not strong enough');
+  }
 
-  // if (existEmail) {
-  //   throw Error('Email already in use');
-  // }
-  // if (!existUsername) {
-  //   throw Error('Username already in use');
-  // }
+  const existEmail = await this.findOne({ email });
+  const existUsername = await this.findOne({ username });
+  if (existEmail) {
+    throw Error('Email already in use');
+  }
+  if (existUsername) {
+    throw Error('Username already in use');
+  }
 
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
   const user = await this.create({ username, email, password: hash });
+  return user;
+};
+
+user.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error('All fields must be filled');
+  }
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error(`email not found`);
+  }
+  const pass = await bcrypt.compare(password, user.password);
+  if (!pass) {
+    throw Error('incorrect password');
+  }
   return user;
 };
 module.exports = mongoose.model('User', user);
